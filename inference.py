a978b61f31f75df008375b57275011db53c0a5c9
lluma <rr123789456tw@gmail.com>
2020-01-06 20:39:44 +0800

    inference

---
+++ inference.py
@@ -0,0 +1,32 @@
+import torch
+
+from network import AvatarNet
+from utils import imload, imsave, maskload
+
+def inference(cfg, args):
+
+    ## Setting inference device
+    device = torch.device('cuda' if torch.cuda.is_available() and cfg.TRAINING.USE_CUDA else 'cpu')
+
+    ## Load checkpoint
+    check_point = torch.load(args.ckpt)
+
+    ## Load network
+    network = AvatarNet(args.layers)
+    network.load_state_dict(check_point['state_dict'])
+    network = network.to(device)
+
+    ## Load target images
+    content_img = imload(args.content_path, .args.imsize, args.cropsize).to(device)
+    style_imgs = [imload(args.style_path, args.imsize, args.cropsize, args.cencrop).to(device) for style in args.style_path]
+    masks = None
+    if args.mask_path:
+        masks = [maskload(mask).to(device) for mask in args.mask_path]
+
+     # stylize image
+    with torch.no_grad():
+        stylized_img =  network(args.content_img, style_imgs, args.style_strength, args.patch_size, args.patch_stride,
+                masks, args.interpolation_weights, False)
+
+    imsave(stylized_img, 'stylized_image.jpg')
+
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse, json
 from yacs.config import CfgNode as CN
 from trainer import train
-
+from inference import inference
 
 def generate_default_configs():
     _C = CN()
@@ -29,9 +29,24 @@ def generate_default_configs():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train or test a network')
+    
+    # Global arguments
     parser.add_argument('config_file', help='config file path')
     parser.add_argument('mode', help='train/test/inference')
     
+    # Arguments for inference
+    parser.add_argument('--imsize', type=int, help='Size for resize image during training', default=512)
+    parser.add_argument('--cropsize', type=int, help='Size for crop image durning training', default=None)
+    parser.add_argument('--cencrop', action='store_true', help='Flag for crop the center rigion of the image, default: randomly crop', default=False)
+    parser.add_argument('--layers', type=int, nargs='+', help='Layer indices to extract features', default=[1, 6, 11, 20])
+    parser.add_argument('--content_path', type=str, help="Test content image path", default='')
+    parser.add_argument('--style_path', type=str, nargs='+', help="Test style image path", default='')
+    parser.add_argument('--mask_path', type=str, nargs='+', help="Mask image for masked stylization", default=None)
+    parser.add_argument('--style-strength', type=float, help='Content vs style interpolation value: 1(style), 0(content)', default=1.0)
+    parser.add_argument('--interpolation-weights', type=float, nargs='+', help='Multi-style interpolation weights', default=None)
+    parser.add_argument('--patch-size', type=int, help='Size of patch for swap normalized content and style features',  default=3)
+    parser.add_argument('--patch-stride', type=int, help='Size of patch stride for swap normalized content and style features',  default=1)
+
     args = parser.parse_args()
     config_file = args.config_file
     mode = args.mode
@@ -48,5 +63,5 @@ if __name__ == '__main__':
 
     if mode == 'train':
         train(cfg)
-    else:
-        test()
+    elif mode == 'inference':
+        inference(cfg, args)
